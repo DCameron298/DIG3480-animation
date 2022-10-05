0ae1cbc130e4c1af087e79a5304c4acc588884e3
Cameron Daniel <dcameron298@gmail.com>
2022-10-04 22:49:32 -0400

    Animation commit

--- My project/Assets/Scripts/Audioplayer.cs
+++ My project/Assets/Scripts/Audioplayer.cs
@@ -10,10 +10,11 @@ public class Audioplayer : MonoBehaviour
 
     public AudioClip musicClipTwo;
    
+    Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,25 +24,26 @@ public class Audioplayer : MonoBehaviour
         {
           musicSource.clip = musicClipOne;
           musicSource.Play();
-
+          anim.SetInteger("State", 1);
          }
 
      if (Input.GetKeyUp(KeyCode.W))
         {
           musicSource.Stop();
-
+          anim.SetInteger("State", 0);
          }
 
      if (Input.GetKeyDown(KeyCode.R))
         {
           musicSource.clip = musicClipTwo;
           musicSource.Play();
+          anim.SetInteger("State", 2);
          }
 
      if (Input.GetKeyUp(KeyCode.R))
         {
           musicSource.Stop();
-
+          anim.SetInteger("State", 0);
          }
 
      if (Input.GetKeyDown(KeyCode.L))
